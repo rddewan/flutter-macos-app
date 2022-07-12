18f9011a5734c6888b4eea1839ab7b1883e89a65
richard <richard@maqe.com>
2022-07-12 08:33:20 +0700

    implemented side bar

--- lib/macos_main.dart
+++ lib/macos_main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:macos_ui/macos_ui.dart';
 
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
       theme: MacosThemeData.light() ,
       darkTheme: MacosThemeData.dark(),
       themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
       home: const MyHomePage(title: 'Home Page'),
     );
   }
@@ -24,15 +26,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -40,68 +33,66 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  int currentIndex = 0;
+  
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+   
+    return MacosWindow(
+      sidebar: Sidebar(
+        top: Row(
+          children: const [
+            Text('MacOS Demo')
           ],
         ),
+        builder: ((context, scrollController) {
+          return SidebarItems(
+            currentIndex: currentIndex, 
+            onChanged: ((value) {
+              setState(() {
+                currentIndex = value;
+              });
+            }),
+            items: const [
+              SidebarItem(
+                leading: MacosIcon(CupertinoIcons.home),
+                selectedColor: Colors.greenAccent,
+                label: Text('Home')
+              ),
+              SidebarItem(
+                leading: MacosIcon(CupertinoIcons.cart),
+                selectedColor: Colors.greenAccent,
+                label: Text('Product')
+              ),
+              SidebarItem(
+                leading: MacosIcon(CupertinoIcons.person),
+                selectedColor: Colors.greenAccent,
+                label: Text('Account')
+              ),
+              SidebarItem(
+                leading: MacosIcon(CupertinoIcons.settings),
+                selectedColor: Colors.greenAccent,
+                label: Text('Setting')
+              ),
+            ], 
+          );
+        }), 
+        minWidth: 200,
+        startWidth: 200,
+        maxWidth: 350,
+        bottom: Row(
+          children: const[
+            MacosIcon(CupertinoIcons.person),
+            Text('demo@gmal.com')
+          ],
+        )
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      child: const Center(
+        child: Text('Home'),
+      ),
+
     );
   }
 }
