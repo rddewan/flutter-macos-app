dff52c718c2c57bbef2d62f8d611e40a217c8bc9
richard <richard@maqe.com>
2022-07-14 08:44:10 +0700

    home screen user list

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:macos_demo/models/user_model.dart';
 import 'package:macos_demo/screens/add_user_screen.dart';
 import 'package:macos_ui/macos_ui.dart';
 
@@ -11,20 +13,62 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late Box<UserModel> userBox;
+
+  @override
+  void initState() {
+    super.initState();
+    userBox = Hive.box('user_box');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MacosScaffold(
+      toolBar: ToolBar(
+        title: Text('Home'),
+        titleWidth: 100,
+        actions: [
+          ToolBarIconButton(
+            label: 'New User', 
+            icon: MacosIcon(CupertinoIcons.add), 
+            showLabel: false,
+            tooltipMessage: 'Add new user',
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (_) => const AddUserScreen()));
+            },
+            )
+        ],
+      ),
       children: [
         ContentArea(builder: (context, scrollController) {
           return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              PushButton(
-                buttonSize: ButtonSize.large,
-                color: Colors.blue,
-                child: Text('New User'), 
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (_) => const AddUserScreen()));
-                },
+              
+              Flexible(
+                child: ListView.builder(
+                  controller: scrollController,
+                  itemCount: userBox.length,
+                  itemBuilder: ((context, index) {
+                    final user = userBox.getAt(index) as UserModel;
+                    return Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(user.name),
+                            Text(user.lastName),
+                            Text(user.gender),
+                            Text(user.email),
+                            Text(user.phone),
+                            Text(user.address),
+                          ],
+                        )
+                    ));
+                    
+                  }),
+                ),
               )
             ],
           );
